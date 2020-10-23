c7d50dc776ad1ee7f7790bc16a14f0c8a1e7c2a1
joseucarvajal <joseucarvajal@gmail.com>
2020-10-23 13:12:01 -0500

    Db Migration and Seeding working with MSSQL

---
+++ SeekQ.Identity/Data/ApplicationDbContextFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SeekQ.Identity.Data
+{
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            optionsBuilder.UseSqlServer("Server=127.0.0.1,1433;Database=SeekQ.NotificationsAndModesSettings;User Id=sa;Password=Password123");
+
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+    }
+}
